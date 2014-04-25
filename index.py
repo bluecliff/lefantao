fa017db6a0a5b9ae423cd787293cb256027261fd
lishuangjiang <li0shuangjiang@gmail.com>
2014-04-25 23:53:24 +0800

    final

--- index.py
+++ index.py
@@ -8,13 +8,22 @@ import time
 import urllib
 import json
 from messageHandler import MessageHandler
-#from bae.core.wsgi import WSGIApplication
+from bae.core.wsgi import WSGIApplication
 
-
+"""
+测试配置
 HOST="127.0.0.1"
 PORT=3306
 USER="root"
 PASSWORD="jsfli"
+DBNAME="uBwCaFPOHivuJfvjeygo"
+CHARSET="utf8"
+"""
+
+HOST="sqld.duapp.com"
+PORT=4050
+USER='nQEFKnIlEoYbdP2TrNOAseE6'
+PASSWORD='mk3BABDi7pGEsA1lewFyIqpIZzfhj72F'
 DBNAME="uBwCaFPOHivuJfvjeygo"
 CHARSET="utf8"
 
@@ -99,7 +108,7 @@ def pullItem():
 
 lft_view=Lefantao.as_view('lft')
 app.add_url_rule('/wx/',view_func=lft_view,methods=['GET','POST'])
-#application = WSGIApplication(app)
+application = WSGIApplication(app)
 
 if __name__ == '__main__':
 #    lft_view=Lefantao.as_view('lft')
